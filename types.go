2ec28dab4c3ffeff13f5f2d2d22e594f0755edf8
Christoph Schott <schott.christoph98@gmx.de>
2021-08-05 20:18:55 +0200

    Added: Messaging / Mail - config

--- types.go
+++ types.go
@@ -2,9 +2,11 @@ package config
 
 // Config holds all Config Models
 type Config struct {
-	App   *AppConfig   `json:"app" yaml:"app"`
-	DB    *DBConfig    `json:"db" yaml:"db"`
-	Cache *CacheConfig `json:"cache" yaml:"cache"`
+	App       *AppConfig       `json:"app" yaml:"app"`
+	DB        *DBConfig        `json:"db" yaml:"db"`
+	Cache     *CacheConfig     `json:"cache" yaml:"cache"`
+	Messaging *MessagingConfig `json:"messaging" yaml:"messaging"`
+	Mail      *MailConfig      `json:"mail" yaml:"mail"`
 }
 
 // AppConfig holds App relevant Configuration
@@ -22,6 +24,7 @@ type DBConfig struct {
 	User    string `json:"user" yaml:"user"`
 	Pass    string `json:"pass" yaml:"pass"`
 	Name    string `json:"name" yaml:"name"`
+	SSL     bool   `json:"ssl" yaml:"ssl"`
 	Enabled bool   `json:"enabled" yaml:"enabled"`
 }
 
@@ -29,5 +32,21 @@ type DBConfig struct {
 type CacheConfig struct {
 	Host    string `json:"host" yaml:"host"`
 	Port    int    `json:"port" yaml:"port"`
+	Enabled bool   `json:"enabled" yaml:"enabled"`
+}
+
+// MessagingConfig holds Message Broker Configuration
+type MessagingConfig struct {
+	Host    string `json:"host" yaml:"host"`
+	Port    int    `json:"port" yaml:"port"`
+	Enabled bool   `json:"enabled" yaml:"enabled"`
+}
+
+// MailConfig holds Mail Server Configuration
+type MailConfig struct {
+	Host    string `json:"host" yaml:"host"`
+	Port    int    `json:"port" yaml:"port"`
+	User    int    `json:"user" yaml:"user"`
+	Pass    int    `json:"pass" yaml:"pass"`
 	Enabled bool   `json:"enabled" yaml:"enabled"`
 }
